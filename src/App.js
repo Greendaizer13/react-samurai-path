761b7da50d606a65942f0c7abd338880bbbb6708
Olga Saveleva <fedchun13@yandex.ru>
2021-03-18 01:29:03 +0300

    Old Header and Technologies components have been removed. There is new markup sketch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
 import React from "react";
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <div>first div</div>
-      <Technologies/>
-    </div>
-  );
+    return (
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src='https://w7.pngwing.com/pngs/737/194/png-transparent-avocado-scalable-graphics-fruit-food-icon-two-avocado-logo-nutrition-cartoon.png' alt={''}/>
+            </header>
+            <nav className={'nav'}>
+                <div><a href={'#1'}>Profile</a></div>
+                <div><a href={'#2'}>Messages</a></div>
+                <div><a href={'#3'}>News</a></div>
+                <div><a href={'#4'}>Music</a></div>
+                <div><a href={'#5'}>Settings</a></div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src={'https://img.chainimage.com/images/moraine-lake-banff-national-park-image.jpg'} alt={''}/>
+                </div>
+                <div>
+                    avatar, description
+                </div>
+                <div>
+                    Posts
+                    <div>New post</div>
+                    <div>Post1</div>
+                    <div>...</div>
+                    <div>PostN</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
