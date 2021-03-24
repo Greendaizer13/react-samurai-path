91562f4d07fb14b05c5f1f3be91bd7ffa90d909e
Olga Saveleva <fedchun13@yandex.ru>
2021-03-25 00:08:54 +0300

    Added routes with links href

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Messages from './components/Messages/Messages';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className={'app-wrapper'}>
-			<Header/>
-			<div className={'app-body'}>
-				<NavBar/>
-				<div className={'content'}>
-					<Profile/>
-					<Messages/>
+		<BrowserRouter>
+			<div className={'app-wrapper'}>
+				<Header/>
+				<div className={'app-body'}>
+					<NavBar/>
+					<div className={'content'}>
+						<Route component={Profile} exact path={'/'}/>
+						<Route component={Profile} path={'/profile'}/>
+						<Route component={Messages} path={'/messages'}/>
+					</div>
 				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,8 +4,12 @@ import styles from './NavBar.module.css';
 const NavBar = () => {
 	return (
 		<nav className={styles['nav']}>
-			<div className={styles['nav__item']}>Profile</div>
-			<div className={styles['nav__item']}>Messages</div>
+			<div className={styles['nav__item']}>
+				<a href={'/profile'}>Profile</a>
+			</div>
+			<div className={styles['nav__item']}>
+				<a href={'/messages'}>Messages</a>
+			</div>
 			<div className={styles['nav__item']}>News</div>
 			<div className={styles['nav__item']}>Music</div>
 			<div className={styles['nav__item']}>Settings</div>
